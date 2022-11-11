5c0290858908e4c6c86f50ed6cbf73776e63da34
dadadon <davianberoni@yahoo.com>
2022-11-11 13:00:09 -0500

    Import Redux Provider and store

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import App from './App';
+import storeConfig from './redux/configureStore';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={storeConfig}>
+      <App />
+    </Provider>
+
   </React.StrictMode>
 );
 
